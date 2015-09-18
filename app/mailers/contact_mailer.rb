e8e540055b6d24cb0991244eea1899ce8602c2db
mattj_kimball <mattkgit@gmail.com>
2015-09-18 16:48:04 +0000

    fixed typo in mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,7 +5,9 @@ class ContactMailer < ActionMailer::Base
         @name = name
         @email = email
         @body = body
-    end
     
     mail(from: email, subject: 'Contact Form Message')
+    
+    end
+    
 end
